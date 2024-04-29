6d97bfea9173bccd2ea0737074b63ca51e7b798c
Jisan Jung <jjjung13@gmail.com>
2024-04-28 22:46:40 -0500

    start pricing section

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,9 @@
 $("#menuBtn").on("click", function() {
     $("#navMobile").slideToggle();
 });
+
+$(".down-chevron").on("click", function() {
+    const clickedParent = $(this).closest('.pricing-each');
+    const priceDetails = clickedParent.find(".price-details");
+    priceDetails.slideToggle();
+});
