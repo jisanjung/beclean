add48cc940b31d8cfa771027ce27b7779bf6bc8c
Jisan Jung <jjjung13@gmail.com>
2024-05-05 22:13:15 -0500

    contact section styling

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,17 @@ $(".down-chevron").on("click", function() {
     const priceDetails = clickedParent.find(".price-details");
     priceDetails.slideToggle();
 });
+
+const contactPreference = document.getElementById('contactPreference');
+contactPreference.addEventListener('change', () => {
+    const textboxEmail = document.getElementById('textboxEmail');
+    const textboxPhone = document.getElementById('textboxPhone');
+    if (contactPreference.value && contactPreference.value === 'Email') {
+        textboxEmail.style.display = 'block';
+        textboxPhone.style.display = 'none';
+    }
+    if (contactPreference.value && contactPreference.value === 'Phone') {
+        textboxEmail.style.display = 'none';
+        textboxPhone.style.display = 'block';
+    }
+});
