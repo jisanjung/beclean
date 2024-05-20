77862af6cafffe1ed34e880b00486000d72030fe
Jisan Jung <jjjung13@gmail.com>
2024-05-19 20:42:35 -0500

    use javascript file in root

---
+++ script.js
@@ -0,0 +1,46 @@
+// view
+$("#menuBtn").on("click", function() {
+    $("#navMobile").slideToggle();
+});
+
+$(".down-chevron").on("click", function() {
+    const clickedParent = $(this).closest('.pricing-each');
+    const priceDetails = clickedParent.find(".price-details");
+    priceDetails.slideToggle();
+});
+
+const contactPreference = document.getElementById('contactPreference');
+contactPreference.addEventListener('change', () => {
+    const textboxEmail = document.getElementById('textboxEmail');
+    const textboxPhone = document.getElementById('textboxPhone');
+    if (contactPreference.value && contactPreference.value === 'Email') {
+        textboxEmail.style.display = 'block';
+        textboxPhone.style.display = 'none';
+    }
+    if (contactPreference.value && contactPreference.value === 'Phone') {
+        textboxEmail.style.display = 'none';
+        textboxPhone.style.display = 'block';
+    }
+
+    if (textboxEmail.style.display === 'block') {
+        textboxEmail.required = true;
+    } else {
+        textboxEmail.required = false;
+    }
+
+    if (textboxPhone.style.display === 'block') {
+        textboxPhone.required = true;
+    } else {
+        textboxPhone.required = false;
+    }
+});
+
+const footerYear = document.getElementById('footerYear');
+const currentYear = new Date().getFullYear();
+footerYear.innerHTML = currentYear;
+
+// smooth scroll
+$('a[href*=\\#]').on('click', function(event){     
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+});
